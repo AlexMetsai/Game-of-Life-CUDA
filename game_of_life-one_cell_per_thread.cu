862c9e7f681c381a99218d66a02c728c43014c3a
Alexandros Metsai <alexmetsai@gmail.com>
2019-05-09 11:50:28 +0300

    Create game_of_life-one_cell_per_thread.cu

---
+++ game_of_life-one_cell_per_thread.cu
@@ -0,0 +1,11 @@
+/* Game of Life - CUDA implementation.
+ * Using one cell per CUDA thread.
+ * Copyright (C) 2019 Alexandros I. Metsai
+ * alexmetsai@gmail.com
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+ 
