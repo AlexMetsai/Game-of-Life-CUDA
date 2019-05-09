ff1d9c24522f8eca338fc44fa860ee4f93817a1a
Alexandros Metsai <alexmetsai@gmail.com>
2019-05-09 11:56:51 +0300

    Code and comment cleanup

--- game_of_life-one_cell_per_thread.cu
+++ game_of_life-one_cell_per_thread.cu
@@ -1,5 +1,6 @@
 /* Game of Life - CUDA implementation.
  * Using one cell per CUDA thread.
+ *
  * Copyright (C) 2019 Alexandros I. Metsai
  * alexmetsai@gmail.com
 
@@ -27,89 +28,68 @@ __global__ void game (int *newer, int *old, int N)
 {
 	
   int index= blockIdx.x*blockDim.x + threadIdx.x;
-  
   int sum=0;
-  
   int i = (int)index/N;
   int j=index%N;
   
+  // Looking on the neighbouring cells is different if the 
+  // cell is at the edges or corners.
   if(i==0){
 	  
-	  //top
-	  
+	  // top
 	  if(j==0){
-		  
 		  sum= old[i*N+(j+1)]+ old[N-1]
 		  +old[N] + old[N+1] + old[2*N-1]
 		  +old[N*(N-1)]+old[N*(N-1) +1] +old[N*N -1];
 		  
 	  }else if(j==(N-1)){
-		  
 		   sum= old[0]+ old[j-1]
 		  +old[j+N] + old[j+1] +old[j+N-1]
 		  +old[N*N-1]+old[N*N-2] + old[N*(N-1)];
 		  
 	  }else{
-		  
-		 
 		 sum= old[j+1] + old[j-1]
 		  +old[j+N] +old[N+(j+1)] +old[N+(j-1)]
 		  +old[N*(N-1)+j]+old[N*(N-1)+(j+1)]+old[N*(N-1)+(j-1)];
-		  
 	  } 
   }
   else if(i==(N-1)){
 	 
 	  if(j==0){		
-		  
-		//bottom-left
-		
+		// bottom-left
 		sum = old[i*N +1] + old[i*N +(N-1)]
 		+old[0] + old[1] +old[(N-1)]
 		+old[(i-1)*N] +old[(i-1)*N +1] +old[(i-1)*N +(N-1)];
 		  
 	  }else if(j==(N-1)){		
-		  
-		  //bottom-right
-		  
+		  // bottom-right
 		  sum = old[i*N] + old[i*N +(j-1)]
 		  +old[N-1] + old[N-2] + old[0]
 		  +old[(i-1)*N +j] +old[(i-1)*N +(j-1)] +old[(i-1)*N];
 		  
 	  }else
 	  {
-		  //bottom
-		  
+		  // bottom
 		  sum= old[i*N +(j+1)] +old[i*N +(j-1)] 
 		  +old[j] +old[j+1] +old[j-1]
 		  +old[(i-1)*N +j] +old[(i-1)*N +(j+1)] +old[(i-1)*N + (j-1)];
-			
 	  }
 	  
   }else if(j==0){			
-	  
-	  //left
-	  
+	  // left
 	  sum= old[i*N +1] +old[i*N + (N-1)]
 	  +old[(i+1)*N] +old[(i+1)*N + 1] + old[(i+1)*N +(N-1)]
 	  +old[(i-1)*N] +old[(i-1)*N +1] +old[(i-1)*N +(N-1)]; 
-	  
   }else if(j==(N-1)){
-	  
-	  /* right */
-	  
+	  // right
 	  sum= old[i*N] + old[i*N +(j-1)]
 	  +old[(i+1)*N +j] +old[(i+1)*N +(j-1)] +old[(i+1)*N]
 	  +old[(i-1)*N +j] +old[(i-1)*N +(j-1)] +old[(i-1)*N];
-	  	  
   }else{
-	 
-	  /* General Case */
-	  
+	  // General Case
 	  sum= old[i*N+(j+1)] + old[i*N +(j-1)]
 	  +old[(i+1)*N + j] +old[(i+1)*N +(j+1)] +old[(i+1)*N +(j-1)]
 	  +old[(i-1)*N + j] +old[(i-1)*N +(j+1)] +old[(i-1)*N +(j-1)];
-	  
   }
   
 	switch(sum){
@@ -141,7 +121,6 @@ int main(){
 	int *temp;
 	
 	int blocks, t, N, count;
-	//int i,j;
 	
 	printf("Set the number of generations\n");
 	scanf("%d", &t);
@@ -149,10 +128,7 @@ int main(){
 	scanf("%d", &N);
 	int size=N*N*sizeof(int);
 	
-	/*
-	 Insert table here
-	 */
-	
+	// Insert table here
 	
 	char filename[20];
 	sprintf(filename, "table%dx%d.bin", N, N);
@@ -160,24 +136,22 @@ int main(){
 	table = (int *)malloc(N*N*sizeof(int));
 	read_from_file(table, filename, N);  	
 	
-
 	printf("This is kernel a\n");
-	
 	printf("The game will be played for %d generations N=%d\n", t, N);
 	
-	//!!!Start Timer!!!
+	// !!! Start Timer !!!
 	gettimeofday (&startwtime, NULL);
 	
-	//Allocate space of new and old in device
+	// Allocate space of new and old in device
 	cudaMalloc(&newer, size);
 	cudaMalloc(&old, size);
 	
-	//copy table
+	// Copy table
 	cudaMemcpy(old, table, size, cudaMemcpyHostToDevice);
 	
 	blocks=(N*N)/(THREADS_PER_BLOCK);
 	
-	//Play game for t generations
+	// Play game for t generations
 	for(count=0;count<t;count++){
 		
 		game<<<blocks, THREADS_PER_BLOCK>>>(newer, old, N);
@@ -189,10 +163,10 @@ int main(){
 		newer=temp;
 	}
 	
-	//Copy back table
+	// Copy back table
 	cudaMemcpy(table, old, size, cudaMemcpyDeviceToHost);
 	
-	//!!!End Timer!!!
+	// !!! End Timer !!!
 	gettimeofday (&endwtime, NULL);
 	  seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
 		      + endwtime.tv_sec - startwtime.tv_sec);
@@ -201,9 +175,9 @@ int main(){
 	
 	save_table(table, N);
 	
+	// Free memory
 	cudaFree(newer);
 	cudaFree(old);
-	
 	free(table);
 	
 	return(0);
@@ -213,11 +187,8 @@ int main(){
 void read_from_file(int *X, char *filename, int N){
 
   FILE *fp = fopen(filename, "r+");
-
   int size = fread(X, sizeof(int), N*N, fp);
-
   printf("elements: %d\n", size);
-
   fclose(fp);
 
 }
@@ -225,17 +196,11 @@ void read_from_file(int *X, char *filename, int N){
 void save_table(int *X, int N){
 
   FILE *fp;
-
   char filename[20];
-
   sprintf(filename, "cuda_a_table%dx%d.bin", N, N);
-
   printf("Saving table in file %s\n", filename);
-
   fp = fopen(filename, "w+");
-
   fwrite(X, sizeof(int), N*N, fp);
-
   fclose(fp);
 
 }
